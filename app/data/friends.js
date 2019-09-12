102f022ec6e0eb2e183281e43f2f329269474035
Linh Nguyen <linh0404@hotmail.com>
2019-09-12 12:03:09 +1000

    added onclick to accept survey results

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendsArray = [
     {
         name: "Chandler Bing",
-        results: []
+        results: [1, 4, 4, 4, 3, 4, 5, 4, 2, 2]
     },
     {
         name: "Monica Geller",
-        results: []
+        results: [2, 4, 5, 4, 4, 2, 2, 3, 3, 2]
     },
     {
         name: "Ross Geller",
-        results: []
+        results: [4, 5, 5, 5, 2, 4, 4, 5, 1, 1]
     },
     {
         name: "Rachel Green",
-        results: []
+        results: [3, 3, 3, 3, 3, 1, 2, 2, 4, 4]
     },
     {
         name: "Joey Tribbiani",
-        results: []
+        results: [2, 2, 2, 2, 1, 3, 3, 1, 5, 3]
     },
     {
         name: "Phoebe Buffay",
-        results: []
+        results: [5, 1, 2, 2, 5, 5, 1, 1, 5, 5]
     },
 ];
 
