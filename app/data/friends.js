ffd4280239388f85442a17881e2b3e63a095321a
Linh Nguyen <linh0404@hotmail.com>
2019-09-14 17:28:46 +1000

    added info tags on each friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,38 @@
 var friendsArray = [
     {
         name: "Chandler Bing",
-        picture: "",
+        picture: "../public/assets/images/chandler.png",
+        info: "If an issue crops up, you and the funniest friend are immediately looking for logic to fix it. Chandler can get so caught up in trying to make others laugh that he forgets to just be himself. Social situations aren't always your thing, but as those who see through your deflections know, you're an intelligent, adaptable problem solver who will do a deep-dive on anything that interests them",
         results: [1, 4, 4, 4, 3, 4, 5, 4, 2, 2]
     },
     {
         name: "Monica Geller",
-        picture: "",
+        picture: "../public/assets/images/monica.png",
+        info: "Order and organisation - two words that describe Monica and yourself. She and her friends are drawn to tradition, which explains why she values the Friends Thanksgiving so highly and loves making decisions. So, take a hint from Phoebe and Monica and only choose this personality type as your bridesmaid if you want to be asked about every meticulous detail",
         results: [2, 4, 5, 4, 4, 2, 2, 3, 3, 2]
     },
     {
         name: "Ross Geller",
-        picture: "",
+        picture: "../public/assets/images/ross.png",
+        info: "You and Central Perk's resident paleontologist are both natural-born leaders who love to plan, set goals, and explain things to others. But your intelelct can sometimes cause you to over-emphasize the rational side of things. You know, like derailing yourself by screaming out, 'We were on a break!' moments before you were about to get your relationship with Rachel like you wanted",
         results: [4, 5, 5, 5, 2, 4, 4, 5, 1, 1]
     },
     {
         name: "Rachel Green",
-        picture: "",
+        picture: "../public/assets/images/rachel.png",
+        info: "The most common word to describe Rachel and her clique is 'popular'. If that's not Rachel, then I dont know what is. Rachel and her friends are often in the spotlight or at the center of social activities and can sometimes feel a little confused when they don't get that attention. They're warmheared and loyal, but they like to receive as much validation as they give out - which is a lot. Make sure not to cross them or you could walk away with a shaved head and one less boyfriend...",
         results: [3, 3, 3, 3, 3, 1, 2, 2, 4, 4]
     }, 
     {
         name: "Phoebe Buffay",
-        picture: "",
+        picture: "../public/assets/images/phoebe.png",
+        info: "Phoebe and her fellows are true free spririts. Warm, optimistic and enthusiastic, they're the life of every party and they never let even the most troubled past keep them down for long. Just give them a guitar and a few mintues and the most mundane moments will be a catchy melody in no time - Smelly Cat",
         results: [5, 1, 5, 2, 1, 5, 5, 1, 5, 5]
     },
     {
         name: "Joey Tribbiani",
-        picture: "",
+        picture: "../public/assets/images/joey.png",
+        info: "Fittingly, Joey and friends label him as the entertainer, which as an actor he would probably love. He tends to love life and make work fun. They're flexible and spontaneous, and they don't mind putting themselves out to risk looking dumb, which is Joey through and through. If they're feeling enthusiastic about something, they want those closest to them to feel the same way, which is probably why Joey's always going on about sandwiches",
         results: [2, 2, 2, 2, 1, 3, 3, 1, 5, 3]
     }
 ];
